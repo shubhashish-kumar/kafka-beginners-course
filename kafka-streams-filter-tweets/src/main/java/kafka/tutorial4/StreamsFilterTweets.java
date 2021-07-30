b4f9db28b4f3abadb1cf22df435af3e26b854382
Shubhashish Kumar <shubhashish.kumar@springernature.com>
2021-07-30 16:57:35 +0530

    kafka streams filter twitter tweeets

---
+++ kafka-streams-filter-tweets/src/main/java/kafka/tutorial4/StreamsFilterTweets.java
@@ -0,0 +1,49 @@
+package kafka.tutorial4;
+
+import com.google.gson.JsonParser;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
+public class StreamsFilterTweets {
+    public static void main(String[] args) {
+        // create properties
+        Properties properties = new Properties();
+        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "demo-kafka-streams");
+        properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+        properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+        // create a topology
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        // input topic
+        KStream<String, String> inputTopic = streamsBuilder.stream("twitter_tweets");
+        KStream<String, String> filteredStream = inputTopic.filter(
+                // filter tweets which has a user with followers over 10000
+                (k, jsonTweet) -> extractUserFollowersInTweet(jsonTweet) > 10000
+        );
+        filteredStream.to("important_tweets");
+        // Build the topology
+        KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
+        // start our streams application
+        kafkaStreams.start();
+    }
+
+    private static JsonParser jsonParser = new JsonParser();
+    private static int extractUserFollowersInTweet(String tweetJson) {
+        // gson library
+        try{
+            return jsonParser.parse(tweetJson)
+                    .getAsJsonObject()
+                    .get("user")
+                    .getAsJsonObject()
+                    .get("followers_count")
+                    .getAsInt();
+        }catch (NullPointerException e){
+            return 0;
+        }
+    }
+}
